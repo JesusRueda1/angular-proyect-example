147bab5f377a9b578c22c1becab3293dd3332917
JesusRueda1 <jdrueda83@misena.edu.co>
2023-02-13 21:42:02 -0500

    Angular con backend

---
+++ servidor/routes/product.js
@@ -0,0 +1,10 @@
+//Rutas
+const express = require('express');
+const router = express.Router();
+
+//api/products
+router.post('/', () => {
+console.log('creating a product...')
+})
+
+module.exports = router;
